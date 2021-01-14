fdcd4da57aacf6ec04e7eb49c2e6767ca1a91098
rembelskiPiotr <rembelskipiotr92@gmail.com>
2021-01-14 13:11:40 +0100

    before code review

--- time_calculator.py
+++ time_calculator.py
@@ -1,53 +1,83 @@
-def add_time(start, duration, day=""):
-  print(start)
-  print(duration)
-  normal_format_hour = convert_to_24_h(start)
-  print(normal_format_hour)
-  minutes_start = convert_to_minutes(normal_format_hour)
-  print(minutes_start)
-  minutes_duration = convert_to_minutes(duration)
-  print(minutes_duration)
-  minutes = minutes_start + minutes_duration
-  print(minutes)
-  days = convert_to_hour(minutes)
-  print(days)
-
-
-
-
-  #return new_time
+def add_time(start, duration, input_day=""):
+  start_minutes = convert_hour_to_minutes(convert_hour_to_24_h_time(start))
+  duration_minutes = convert_hour_to_minutes(duration)
+  sum_of_minutes = start_minutes + duration_minutes
+  raw_time, past_days = convert_minutes_to_hour(sum_of_minutes)
+  raw_time_converted = convert_hour_to_12_h_time(raw_time)
+  
+  if input_day == "":
+    if past_days == 0: 
+      new_time = raw_time_converted
+    elif past_days > 0:
+      past_days_text = display_past_days(past_days)
+      new_time = raw_time_converted +" "+ past_days_text
+  else:
+    input_day_index = display_week_day_index(input_day)
+    new_weekday = display_weekday(calculate_new_week_day(past_days,input_day_index))
+    if past_days == 0: 
+      new_time = raw_time_converted +", "+ input_day
+    elif past_days > 0:
+      past_days_text = display_past_days(past_days)
+      new_time = raw_time_converted +", "+ new_weekday +" "+ past_days_text
+  return new_time
   
 #format HH:MM PM
-def convert_to_24_h(hour):
+def convert_hour_to_24_h_time(hour):
+  hours, minutes = split_hour_by_colon(hour)
   if "AM" in hour:
-    if "12" in hour[:2]:
-      return "00" + hour[2:-2]
-    return hour[:-2]
+    if hours == 12:
+      return "00" + minutes
+    return "{:d}:{:02d}".format(hours, minutes)
   elif "PM" in hour:
-    if "12" in hour[:2]:
-      return hour[:-2]
-    return str(int(hour[:2]) + 12) + hour[2:5] 
+    if hours == 12:
+      return "{:d}:{:02d}".format(hours, minutes)
+    return "{:d}:{:02d}".format(hours+12, minutes) 
 
-def convert_to_minutes(hour):
-  hours, minutes = split_by_colon(hour)
-  hours = get_rid_of_zero(hours)
-  minutes = get_rid_of_zero(minutes)
+def convert_hour_to_12_h_time(hour):
+  hours, minutes = split_hour_by_colon(hour)
+  if hours > 12:
+    hours = hours - 12
+    return "{:d}:{:02d} PM".format(hours, minutes)
+  elif hours == 12:
+    return "{:d}:{:02d} PM".format(hours, minutes)
+  elif hours == 0:
+    hours = 12
+    return "{:d}:{:02d} AM".format(hours, minutes)
+  else:
+    return "{:d}:{:02d} AM".format(hours, minutes)
+
+def convert_hour_to_minutes(hour):
+  hours, minutes = split_hour_by_colon(hour)
   minutes += hours * 60
   return minutes
 
-def split_by_colon(hour):
+def split_hour_by_colon(hour):
   elements = hour.split(":")
-  hours = elements[0]
-  minutes = elements[1]
+  hours = int(elements[0])
+  right_side = elements[1].split(" ")
+  minutes = int(right_side[0])
   return hours, minutes
 
-def get_rid_of_zero(number):
-  if "0" in number:
-    return int(number[1:])
-  else:
-    return int(number)
-
-def convert_to_hour(minutes):
+def convert_minutes_to_hour(minutes):
   hours, minutes = divmod(minutes, 60)
   days, hours = divmod(hours, 24)
-  return "{:d}:{:02d}:{:02d}".format(days, hours, minutes)
+  return "{:d}:{:02d}".format(hours, minutes), days
+
+def display_past_days(days):
+  if days == 1:
+    return "(next day)"
+  else:
+    return "({} days later)".format(days)
+
+def display_week_day_index(day):
+  weekdays = ("monday","tuesday","wednesday","thursday","friday","saturday","sunday")
+  day = day.lower()
+  for index, weekday in enumerate(weekdays):
+    if day == weekday: return index  
+
+def calculate_new_week_day(past_days,input_day_index):
+  return divmod(past_days + input_day_index,7)[1]
+
+def display_weekday(day_index):
+  weekdays = ("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday")
+  return weekdays[day_index]
